603e086ad3ea098cc17e9c597801fe99ac1e11a6
marecki <marekmarkowskimm@gmail.com>
2024-04-24 07:41:11 +0200

    PROJECT BEGIN:
    Configuration of timer7. On that timer we set 1000 [Hz] freq and activate overflow interrupt.
    Testing on LD2.

---
+++ Core/Inc/software_timers.h
@@ -0,0 +1,13 @@
+/*
+ * software_timers.h
+ *
+ *  Created on: Apr 24, 2024
+ *      Author: mm
+ */
+
+#ifndef INC_SOFTWARE_TIMERS_H_
+#define INC_SOFTWARE_TIMERS_H_
+
+
+
+#endif /* INC_SOFTWARE_TIMERS_H_ */
---
+++ Core/Src/software_timers.c
@@ -0,0 +1,16 @@
+/*
+ * software_timers.c
+ *
+ *  Created on: Apr 24, 2024
+ *      Author: mm
+ */
+
+
+#include "tim.h"
+
+void init_software_timers(void)
+{
+
+
+
+}
